3af9951b2e795a9d32dfab3358120aa8a9367483
NameegPlanov <planovnamiq27@gmail.com>
2024-05-06 23:24:25 +0400

    dubizzle car data analysis 2nd part

--- dubizzle car data analysis/data.py
+++ dubizzle car data analysis/data.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from textblob import TextBlob
 
 data=pd.read_csv('dubizzle car data analysis/dubizzle_cars_dataset.csv')
 #print('Number of registered cars null values-{}'.format(data.shape[0]))
@@ -45,5 +46,65 @@ plt.xlabel('Age of cars',labelpad=14)
 plt.ylabel('Number of cars',labelpad=14)
 plt.show()
 '''
-info3=data['body_type'].value_counts()
-print('Number of cars based on their body types:\n{}'.format(info3))
+#info3=data['body_type'].value_counts()
+#print('Number of cars based on their body types:\n{}'.format(info3))
+kilometers_int=[0,2000,5000,10000,20000,40000,100000,400000,1000000,2000000,float('inf')]
+kilometers_str=['0-2k','2k-5k','5k-10k','10k-20k','20k-40k','40k-100k','100k-400k','400k-1M','1M-2M','2M+']
+#data['driven_kilometers_category']=pd.cut(data['kilometers'],bins=kilometers_int,labels=kilometers_str,right=False)
+
+'''
+sns.countplot(x='engine_capacity_cc',data=data,color='grey',hue='warranty')
+plt.title('Analyzing the number of cars based on engine capaicty and warranty')
+plt.xlabel('Egine capacity',labelpad=15)
+plt.ylabel('Number of cars',labelpad=15)
+plt.xticks(fontsize=8)
+plt.legend(loc='upper right')
+plt.show()
+'''
+data['Price per Kilometer']=data['price']/data['kilometers']
+#print(data['Price per Kilometer'])
+'''
+price_year=data.groupby('year')['price'].mean().reset_index()
+sns.lineplot(x='year',y='price',data=price_year,color='blue',marker='o')
+plt.title('Analyzing the average price over years')
+plt.xlabel('Year',labelpad=14)
+plt.ylabel('Average price',labelpad=14)
+plt.show()
+'''
+'''
+info4=data.groupby('area_name')['price'].mean().reset_index()
+sorted_info4=info4.sort_values(by='price',ascending=False).head(15)
+sns.barplot(x='area_name',y='price',data=sorted_info4,color='grey')
+plt.title('Visualization the distribution of average prices of cars based on area')
+plt.xlabel('Area',labelpad=14)
+plt.ylabel('Average Price',labelpad=14)
+plt.xticks(rotation=30,fontsize=8)
+plt.show()
+'''
+'''
+arr1=data[['kilometers','engine_capacity_cc','horsepower','no_of_cylinders']]
+target_value=data['price']
+arr1_encoded=pd.get_dummies(arr1)
+x_train,x_test,y_train,y_test=train_test_split(arr1_encoded,target_value,random_state=42,test_size=0.3)
+model=LinearRegression()
+model.fit(x_train,y_train)
+prediction=model.predict(x_test)
+
+sns.lineplot(x=range(len(y_test)),y=y_test.values,label='Actual price',color='green')
+sns.lineplot(x=range(len(prediction)),y=prediction,label='Predicted Price',color='blue')
+plt.title('Figuring out the actual price and predicted price based on features of each car')
+plt.show()
+'''
+def sentiment_analysis(text):
+    blob=TextBlob(text)
+    sentiment=blob.sentiment.polarity
+    if sentiment<0:
+        return 'Negative'
+    elif sentiment>0:
+        return 'Positive'
+    else:
+        return 'Neutral'
+data['Exterior color sentiment']=data['exterior_color'].apply(sentiment_analysis)
+data['Interior color sentiment']=data['interior_color'].apply(sentiment_analysis)
+print(data['Exterior color sentiment'].head(10))
+print(data['Interior color sentiment'].head(10))
