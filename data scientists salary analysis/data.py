176b5712778164de034fa6e5afb58be620abd6a3
NameegPlanov <planovnamiq27@gmail.com>
2024-05-26 23:36:05 +0400

    data scientists salary analysis 2nd part

--- data scientists salary analysis/data.py
+++ data scientists salary analysis/data.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from textblob import TextBlob
+from scipy.stats import ttest_ind,f_oneway
 
 
 data=pd.read_csv('data scientists salary analysis/salaries _2.csv')
@@ -25,6 +26,7 @@ numerical_columns=data.select_dtypes(include='number')
 
 data['difference in salaries (usd and other currencies)']=data['salary_in_usd']-data['salary']
 #print(data['difference in salaries (usd and other currencies)'].head(10))
+
 '''
 info1=data.groupby('job_title')['salary_in_usd'].mean().head(10).reset_index()
 info1['salary_in_usd']=info1['salary_in_usd'].astype('int')
@@ -36,22 +38,85 @@ info2['salary_in_usd']=info2['salary_in_usd'].astype('int')
 print('Average salary for each experience level\n{}'.format(info2))
 print('------------------------------------------------------------')
 
-info2=data.groupby('employment_type')['salary_in_usd'].mean().head(10).reset_index()
-info2['salary_in_usd']=info2['salary_in_usd'].astype('int')
-print('Average salary for each employment type\n{}'.format(info2))
+info3=data.groupby('employment_type')['salary_in_usd'].mean().head(10).reset_index()
+info3['salary_in_usd']=info3['salary_in_usd'].astype('int')
+print('Average salary for each employment type\n{}'.format(info3))
 '''
 info4=data.groupby('job_title')['remote_ratio'].mean().head(15)
 #print(info4)
 
 
-#info3=data.groupby('company_location')['salary_in_usd'].mean().head(10).reset_index()
-#info3['salary_in_usd']=info3['salary_in_usd'].astype('int')
-#print('Average salary for each location of the company\n{}'.format(info3))
+#info5=data.groupby('company_location')['salary_in_usd'].mean().head(10).reset_index()
+#info5['salary_in_usd']=info5['salary_in_usd'].astype('int')
+#print('Average salary for each location of the company\n{}'.format(info5))
 
-#info3=data.groupby('employee_residence')['salary_in_usd'].mean().head(10).reset_index()
-#info3['salary_in_usd']=info3['salary_in_usd'].astype('int')
-#print('Average salary for each employee residence\n{}'.format(info3))
+#info6=data.groupby('employee_residence')['salary_in_usd'].mean().head(10).reset_index()
+#info6['salary_in_usd']=info6['salary_in_usd'].astype('int')
+#print('Average salary for each employee residence\n{}'.format(info6))
 
-plt.plot(data['salary_in_usd'],kde=True,bins=12,color='green',binwidth=2)
-plt.title('Analyzing the distribution of salaries')
-plt.show()
+#print(numerical_columns.corr())
+#sns.heatmap(numerical_columns.corr(),annot=True)
+#plt.title('Discovering the relationship between numerical values with heatmap')
+#plt.show()
+
+#sns.boxplot(x='experience_level',y='salary_in_usd',data=data,color='green')
+#plt.title('Analyzing the salary distribution across different experience levels with boxplot')
+#plt.xlabel('Experience level',labelpad=14)
+#plt.ylabel('Salary in USD',labelpad=14)
+#plt.show()
+
+#sns.histplot(data['remote_ratio'],kde=True,bins=12,binwidth=2)
+#plt.title('Visualizing the distribution of remote ratio with histplot')
+#plt.xlabel('Remote ratio',labelpad=14)
+#plt.ylabel('')
+#plt.show()
+
+#info7=data['employment_type'].value_counts()
+#plt.pie(info7,labels=info7.index,autopct='%2.1f%%')
+#plt.title('Creating a pie chart of employment types')
+#plt.show()
+
+data['remote_ratio_category']=data['remote_ratio'].apply(lambda x: 'No Remote' if x==0 else ('Hybrid' if x==50 else 'Remote'))
+info8=data.groupby('remote_ratio_category')['salary_in_usd'].mean().reset_index()
+#sns.barplot(x='remote_ratio_category',y='salary_in_usd',data=info8,color='green')
+#plt.title('Comparing the average salary for each remote ratio category')
+#plt.xlabel('Remote Ratio Category',labelpad=14)
+#plt.ylabel('Average salary in usd',labelpad=14)
+#plt.show()
+
+info9=data.groupby('company_size')['salary_in_usd'].mean().reset_index()
+#sns.barplot(x='company_size',y='salary_in_usd',data=info9,color='green')
+#plt.title('Comparing the average salary for each company size')
+#plt.xlabel('Company size',labelpad=14)
+#plt.ylabel('Average salary in usd',labelpad=14)
+#plt.show()
+
+arr1=data[['experience_level','company_size']]
+target=data['salary_in_usd']
+arr1_normalized=pd.get_dummies(arr1,drop_first=True)
+x_train,x_test,y_train,y_test=train_test_split(arr1_normalized,target,random_state=42,test_size=0.3)
+model=LinearRegression()
+model.fit(x_train,y_train)
+prediction=model.predict(x_test)
+
+#sns.lineplot(x=range(len(y_test)),y=y_test.values,label='Actual Salary in USD')
+#sns.lineplot(x=range(len(prediction)),y=prediction,label='Predicted Salary in USD')
+#plt.title('Actual Salary vs Predicted Salary')
+#plt.show()
+
+info10=data.groupby('work_year')['salary_in_usd'].mean().reset_index()
+#plt.plot(info10['work_year'],info10['salary_in_usd'],marker='o',color='blue',linestyle='-')
+#plt.title('Performing a time series analysis in salaries')
+#plt.xlabel('Years',labelpad=14)
+#plt.ylabel('Average salary in USD',labelpad=14)
+#plt.show()
+
+
+info11=data['experience_level'].unique()
+salary1=[data[data['experience_level']==level]['salary'] for level in info11]
+ttest,p_value=f_oneway(*salary1)
+alpha=0.05
+if p_value<alpha:
+    print('There is significant difference in salary between various experience levels')
+else:
+    print('There is no significant difference in salary between various experience levels')
--- python/python.py
+++ python/python.py
@@ -28,29 +28,40 @@ arr4.index=['Gedebey','Qusar','Lerik']
 print(arr4)
 '''
 
-data={'Names':['Namiq','Ayxan','Ali','Ilkin','John'],
-      'Ages':[21,17,21,23,22],
-      'Cities':['Baku','Baku','Ganja','Agdam','Toronto']}
+#data={'Names':['Namiq','Ayxan','Ali','Ilkin','John'],
+#      'Ages':[21,17,21,23,22],
+#      'Cities':['Baku','Baku','Ganja','Agdam','Toronto']}
+#
+#dataframe1=pd.DataFrame(data)
+#dataframe1.index.name='Info about students'
+##print(dataframe1)
+##print(dataframe1.head(2))
+#frame2=pd.DataFrame(data,index=['one','two','three','four','five'])
+##print(frame2)
+##print(frame2.index)
+##print(frame2['Names'])
+##print(frame2.loc['two'])
+#numbers=[91,92,93,94,95]
+#frame2['Score']=[i for i in numbers]
+#frame2.columns=['Name','Age','City','Score']
+##print(frame2)
+#
+#values=pd.Series([4.5,5,2.5,3.4,4.1],index=['one','two','three','four','five'])
+#frame2['Star']=values
+##print(frame2)
+#frame2['isGraduated']=frame2.Score==99
+##print(frame2)
+#del frame2['isGraduated']
+#print(frame2.columns)
+#print(frame2.values)
 
-dataframe1=pd.DataFrame(data)
-dataframe1.index.name='Info about students'
-#print(dataframe1)
-#print(dataframe1.head(2))
-frame2=pd.DataFrame(data,index=['one','two','three','four','five'])
-#print(frame2)
-#print(frame2.index)
-#print(frame2['Names'])
-#print(frame2.loc['two'])
-numbers=[91,92,93,94,95]
-frame2['Score']=[i for i in numbers]
-frame2.columns=['Name','Age','City','Score']
-#print(frame2)
 
-values=pd.Series([4.5,5,2.5,3.4,4.1],index=['one','two','three','four','five'])
-frame2['Star']=values
-#print(frame2)
-frame2['isGraduated']=frame2.Score==99
-#print(frame2)
-del frame2['isGraduated']
-print(frame2.columns)
-print(frame2.values)
+numbers=[x for x in range(10)]
+obj=pd.Series(numbers,index=['d','b','f','e','a','g','j','c','l','i'])
+
+#print(obj.sort_index())
+frame=pd.DataFrame(np.arange(9).reshape((3,3)),index=['1st row','2nd row','3rd row'],columns=['a','b','c'])
+#print(frame)
+
+obj2=pd.Series([1,2,3,4,5,6,7])
+print(obj2.rank())
