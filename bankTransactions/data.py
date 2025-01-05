b25a663eee5e3b8ecbfd27c8eb36290cf38f403e
NameegPlanov <planovnamiq27@gmail.com>
2025-01-05 22:59:06 +0400

    latest updates

--- bankTransactions/data.py
+++ bankTransactions/data.py
@@ -6,10 +6,9 @@ from sklearn.linear_model import LinearRegression
 
 
 data = pd.read_csv('bankTransactions/banktransactions.csv')
-#print(data.head(10))
+print(data.head(10))
 
-#Check the data types of each column
-#print(data.dtypes) 
+ 
 data['TransactionDate'] = pd.to_datetime(data['TransactionDate'],format='mixed')
 data['TransactionDay'] = data['TransactionDate'].dt.day_of_week
 data['TransactionDay'] = data['TransactionDay'].map({
@@ -22,7 +21,7 @@ data['TransactionMonth'] = data['TransactionDate'].dt.month
 data['TransactionMonth'] = data['TransactionMonth'].map({
     1:'January',2:'February',3:'March',4:'April',5:'May',
     6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',
-    12:'DEcember'
+    12:'December'
 })
 
 data['PreviousTransactionDate'] = pd.to_datetime(data['PreviousTransactionDate'],format='mixed')
@@ -34,15 +33,14 @@ data['PreviousTransactionDate'] = data['PreviousTransactionDate'].map({
 })
 
 
-#Identify and remove duplicate rows
-#print('Is there any duplicated row?{}'.format(data.duplicated().values.any()))
 
-#Identify missing values in each column and handle them (e.g., impute, drop).
-#print('Is there any duplicated row?{}'.format(data.isnull().values.any()))
+print('Is there any duplicated row? {}'.format(data.duplicated().values.any()))
 
-#Calculate the mean, median, and standard deviation for TransactionAmount, CustomerAge, and AccountBalance
 
-'''
+print('Is there any duplicated row? {}'.format(data.isnull().values.any()))
+
+
+
 print('The mean value of TransactionAmount - {}'.format(data['TransactionAmount'].mean()))
 print('The median of TransactionAmount - {}'.format(data['TransactionAmount'].median()))
 print('The Standard Deviation of TransactionAmount - {}'.format(data['TransactionAmount'].std()))
@@ -55,14 +53,14 @@ print('-----------------------------------------------')
 
 print('The mean value of AccountBalance - {}'.format(data['AccountBalance'].mean()))
 print('The median of AccountBalance - {}'.format(data['AccountBalance'].median()))
-print('The Standard Deviation of AccountBalance - {}'.format(data['AccountBalance'].std()))'''
+print('The Standard Deviation of AccountBalance - {}'.format(data['AccountBalance'].std()))
 
 
-#Find the number of unique values in each column
-#print(data.nunique())
 
-#Plot histograms for TransactionAmount, CustomerAge, and TransactionDuration.
-'''
+print(data.nunique())
+
+
+
 sns.histplot(data['TransactionAmount'],kde=True,bins=11,color='green')
 plt.title('Histplot of the distribution of The Amount of Transaction')
 plt.tight_layout()
@@ -79,18 +77,18 @@ plt.title('Histplot of the distribution of The Duration of Transactions')
 plt.xlabel('Durations of Transactions')
 plt.tight_layout()
 plt.show()
-'''
+
 
 numerical_cols = data.select_dtypes(include='number')
-'''
+
 sns.jointplot(x='AccountBalance',y='TransactionAmount',data=data,color='orange',kind='hex')
 plt.title('Joint Plot of 2 numerical columns')
 plt.tight_layout()
 plt.show()
-'''
-#Group transactions by TransactionDate and calculate daily total transaction amounts.
-#dailyTransactions = data.groupby('TransactionDay').size().reset_index(name='TotalTransactions')
-'''sns.barplot(x='TransactionDay',y='TotalTransactions',data=dailyTransactions,color='green')
+
+
+dailyTransactions = data.groupby('TransactionDay').size().reset_index(name='TotalTransactions')
+sns.barplot(x='TransactionDay',y='TotalTransactions',data=dailyTransactions,color='green')
 plt.title('Transactions per day')
 plt.xlabel('Total number of Transactions')
 plt.tight_layout()
@@ -104,103 +102,103 @@ plt.title('Transactions per month')
 plt.xlabel('Months')
 plt.ylabel('Number of transactions')
 plt.show()
-'''
 
 
-#Identify the top 10 locations by the number of transactions.
-#1st method
-topLocations = data['Location'].value_counts().head(10)
-#print(topLocations)
 
-topLocations2 = data.groupby(['Location','TransactionType']).size().reset_index(name='TotalTransactions')
-sortedlocations = topLocations2.nlargest(10,'TotalTransactions')
+
+topLocations = data.groupby(['Location','TransactionType']).size().reset_index(name='TotalTransactions')
+sortedlocations = topLocations.nlargest(10,'TotalTransactions')
+print(sortedlocations)
+
+
+
 sns.barplot(x='Location',y='TotalTransactions',hue='TransactionType',data=sortedlocations,color='blue')
 plt.title('Top Locations for the Money Transaction')
 plt.legend(loc='upper right')
 plt.show()
-#print(sortedlocations)
+print(sortedlocations)
 
-#Find the average TransactionAmount per customer.
+
 
 avgtransaction = data.groupby('TransactionID')['TransactionAmount'].mean().reset_index().head(15)
-#print(avgtransaction)
+print(avgtransaction)
 
-#Extract the hour from TransactionDate and identify peak transaction hours.
+
 
 data['TransactionHour'] = data['TransactionDate'].dt.hour
-'''
 transactionPerHour = data.groupby('TransactionHour').size().reset_index(name='TotalTransactions').head(3)
 sns.barplot(x='TransactionHour',y='TotalTransactions',data=transactionPerHour,color='blue')
 plt.title('Analyzing  3 peakest transaction hours')
 plt.xlabel('Hours')
 plt.ylabel('Number of Transactions')
-plt.show()'''
+plt.show()
 
-#Create a pie chart showing the distribution of TransactionType
+
 typesOfTransaction = data['TransactionType'].value_counts()
-#plt.pie(typesOfTransaction,labels=typesOfTransaction.index,autopct='%2.1f%%')
-#plt.show()
+plt.pie(typesOfTransaction,labels=typesOfTransaction.index,autopct='%2.1f%%')
+plt.title('Types of Transaction')
+plt.show()
 
-#Analyze the popularity of each Channel
+
 channelInfo = data['Channel'].value_counts()
-#print(channelInfo)
+print(channelInfo)
 
-#print(numerical_cols.corr())
+
 
 specificCols= data[['CustomerAge','LoginAttempts','AccountBalance']]
-#sns.heatmap(specificCols,cmap='Blues')
-#plt.show()
 
-#Identify accounts with unusually high LoginAttempts.
+
+
 maxAttempts = 3
-#print('Number of Individuals who attempted more than 3 times-{}'.format(data[data['LoginAttempts']>3]['LoginAttempts'].sum()))
-#print('Details of some of them\n{}'.format(data[data['LoginAttempts']>3][['TransactionID','AccountID','TransactionType','LoginAttempts']].head(10)))
+print('Number of Individuals who attempted more than 3 times-{}'.format(data[data['LoginAttempts']>3]['LoginAttempts'].sum()))
+print('Details of some of them\n{}'.format(data[data['LoginAttempts']>3][['TransactionID','AccountID','TransactionType','LoginAttempts']].head(10)))
 
 
-#Detect transactions with unusually long or short durations
+
 minDuration = 150
 maxDuration = 260
-#print('Number of People who spent less time than required-{}'.format(len(data[data['TransactionDuration']<minDuration])))
-# print('Number of People who spent more time than required-{}'.format(len(data[data['TransactionDuration']<maxDuration])))
+print('Number of People who spent less time than required-{}'.format(len(data[data['TransactionDuration']<minDuration])))
+print('Number of People who spent more time than required-{}'.format(len(data[data['TransactionDuration']<maxDuration])))
 
 
-#Identify customers with transactions from multiple IP addresses.
+
 customersWithUniqueIPs = data.groupby('AccountID')['IP Address'].nunique().reset_index()
 customersWithUniqueIPs.columns = ['AccountID', 'DistinctIPCount']
-customersWithMultipleIPs = customersWithUniqueIPs[customersWithUniqueIPs['DistinctIPCount']>1]['AccountID'].head(20)
-#print(customersWithMultipleIPs)
+customersWithMultipleIPs = customersWithUniqueIPs[customersWithUniqueIPs['DistinctIPCount']>1]['AccountID'].head(15)
+print(customersWithMultipleIPs)
 
 
-#Segment customers based on their CustomerAge, TransactionAmount
+
 
 ages = [18,35,50,65,float('inf')]
 age_group = ['18-35','35-50','50-65','65+']
 data['AgeGroup'] = pd.cut(data['CustomerAge'],bins=ages,labels=age_group,right=False)
+
 transactions =[0,100,200,400,800,1600,float('inf')]
 transactionGroup = ['0 transaction','very little transaction','small transaction',
 'medium transaction','large transaction','extremely large transaction']
 data['TransactionGroup'] = pd.cut(data['TransactionAmount'],bins=transactions,labels=transactionGroup,right=False)
+print(data['TransactionGroup'].head(10))
 
-
-#Identify the top merchants by transaction volume
 
 topMerchants = data.nlargest(5,'TransactionAmount')[['TransactionID','AccountID','TransactionAmount']]
-#print(topMerchants)
+print(topMerchants)
 
 
-#Calculate the average transaction amount per day for each account.
+
 AvgTransaction = data.groupby('TransactionDay')['TransactionAmount'].mean().reset_index()
-#print(AvgTransaction)
+print(AvgTransaction)
 
 
 
-#Analyze how AccountBalance changes after each transaction.
+
 data['ChangeInBalance'] = data.groupby('AccountID')['TransactionAmount'].diff()
-#print(data[data['ChangeInBalance']>0][['AccountID','TransactionDate','TransactionAmount', 'AccountBalance', 'ChangeInBalance']].head(15))
+print(data[data['ChangeInBalance']>0][['AccountID','TransactionDate','TransactionAmount', 'AccountBalance', 'ChangeInBalance']].head(15))
 
-#Identify which devices are most commonly used for transactions.
+
 TopDevices = data['DeviceID'].value_counts().head(5).index
 filteredData = data[data['DeviceID'].isin(TopDevices)]
+
 sns.countplot(x='DeviceID',hue='TransactionType',data=filteredData,color='green')
 plt.title('Top 5 devices which were used for Transactions')
 plt.xlabel('Devices')
